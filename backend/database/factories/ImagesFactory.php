a24b90329e2fb430b9b8b2763b1d749cc5edcece
nhatha240 <45021699+nhatha240@users.noreply.github.com>
2021-02-27 18:10:43 +0700

    áda

--- backend/database/factories/ImagesFactory.php
+++ backend/database/factories/ImagesFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\images;
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ImagesFactory extends Factory
@@ -13,7 +14,9 @@ class ImagesFactory extends Factory
      * @var string
      */
     protected $model = images::class;
-
+    protected $ = User::factory()
+    ->has(Post::factory()->count(3))
+    ->create();
     /**
      * Define the model's default state.
      *
@@ -22,7 +25,7 @@ class ImagesFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'id_products' => $this->faker->
         ];
     }
 }
